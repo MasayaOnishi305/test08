d9f7f78b8b6a37994886468dab93f00e287c4380
MasayaOnishi305 <72341237+MasayaOnishi305@users.noreply.github.com>
2021-01-30 06:54:46 +0900

    PC・SPともに完了

---
+++ js/jQuery.js
@@ -0,0 +1,10 @@
+$(function () {
+  //hb-menu
+  $('.menu-btn').on('click', function () {
+    $('.hb-menu').toggleClass('is-none');
+  });
+  $('.hb-menu-btn').on('click', function () {
+    $('.hb-menu').toggleClass('is-none');
+  });
+  
+});
